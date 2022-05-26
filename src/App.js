d6802ea834e6a693daf915295d1d9d14384d2aed
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-05-26 10:41:26 +0200

    modalità dark con useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+
 
 function App() {
+
+  const [styleMode, setStyleMode] = useState('light')
+
+  useEffect(() => {
+    document.title = "Dark mode"
+  }, []);
+
+  const switchMode = () => {
+    setStyleMode((oldValue) => {
+      return oldValue === 'light' ? 'dark' : 'light';
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={['App', styleMode].join(" ")}>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-lg-12">
+            <h4>Titolo del paragrafo</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <button className="btn btn-success" onClick={switchMode}>{styleMode === 'light' ? "Modalità dark" : "Modalità light"}</button>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
